95ea71f1463e4ce61131ed3865043af99f685286
Armando Duran Salvador <iccarmandoduran@gmail.com>
2017-07-19 12:55:08 -0500

    Practica Hello World Web service REST with Jersey

---
+++ src/main/java/com/beeva/rest/Saludador.java
@@ -0,0 +1,20 @@
+package com.beeva.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class Saludador {
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Hola : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
